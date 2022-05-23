ebe45e5f9d320018875fc097cc28b038a5531305
cadiablue1 <cadiablue1@naver.com>
2022-05-23 18:23:28 +0900

    22.05.23

---
+++ test2/src/test2/MultiplicationTable.java
@@ -0,0 +1,29 @@
+package test2;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+	static int test(int a) {
+		do {
+			System.out.print("단입력:");
+			
+		}while();
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		while(true) {
+		System.out.print("구구단 몇 단:");
+		int a = input.nextInt();
+		if(!(a ==0)||a ==1) {
+			System.out.println(a+"단 출력");
+			for(int j=1 ; j<=9 ;j++) {
+				System.out.printf("%2d *%2d =  %2d\n",a,j,a*j);
+			}
+		}else {
+			System.out.println("프로그램을 종료합니다");
+			break;
+		}
+	 }
+	}
+}
